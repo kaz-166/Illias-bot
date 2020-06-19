dfc9bce699cd0bcf2e6b7e0352c278da5240cb0b
kazuma <wanwan@super.com>
2020-06-19 12:33:48 +0900

    [update] OdysseaControllerのインターフェイスを変更

--- app/controllers/odyssea_controller.rb
+++ app/controllers/odyssea_controller.rb
@@ -1,24 +1,8 @@
 class OdysseaController < ApplicationController
 	
 	def callback
-		message = params[:text]
-		reply = parse_command(message)
-		render json: { status: 'SUCCESS', message: reply }
+		status, reply = MethodSelecter.exec(params)
+		render json: { status: status, message: reply }
 	end
-
-	private
-	def parse_command(message)
-		if GreetingMethods.matching?(message)
-			GreetingMethods.exec_command_greeting
-		elsif WeatherMethods.matching?(message)
-			WeatherMethods.exec_command_weather(message)
-		elsif ManualMethods.matching?(message)
-			ManualMethods.exec_commamd_manual
-		elsif ReminderMethods.matching?(message)
-			ReminderMethods.exec_commamd_reminder(message)
-		else
-			# コマンド不一致の場合は何も返さない
-		end
-	end 
     
 end
