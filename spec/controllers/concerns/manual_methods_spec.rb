56bf852bf8453b09eaed2288d46d1c9040f178e7
kazuma <wanwan@super.com>
2020-06-20 20:47:32 +0900

    [add] manual機能のテストコードの追加

--- spec/controllers/concerns/manual_methods_spec.rb
+++ spec/controllers/concerns/manual_methods_spec.rb
@@ -8,3 +8,29 @@ RSpec.describe LinebotController, type: :controller do
 		end
 	end
 end
+
+RSpec.describe OdysseaController, type: :controller do 	
+	describe 'Ilias-Botは' do
+		context 'id=2のJSONをPOSTメソッドで受け取ったとき' do
+			it "リクエストが成功する" do
+				post :callback, body: "{\"id\": \"2\"}", as: :json
+				expect(response.status).to eq(200)
+			end
+			it "JSON['status']を'SUCCESS'にして返す" do
+				post :callback, body: "{\"id\": \"2\"}", as: :json
+				json = JSON.parse(response.body)
+				expect(json['status']).to eq 'SUCCESS'
+			end
+			it "JSON['message']を'適切なメッセージ'にして返す" do
+				post :callback, body: "{\"id\": \"2\"}", as: :json
+				json = JSON.parse(response.body)
+				expect(json['message']).to include 'ユーザズマニュアルのリンクを貼っておきますね。'
+			end
+			it "JSON['expression']を'happy'にして返す" do
+				post :callback, body: "{\"id\": \"2\"}", as: :json
+				json = JSON.parse(response.body)
+				expect(json['expression']).to eq 'happy'
+			end
+		end
+	end
+end
